c359db612e964b0cce7bb1532f47fbbafda28ab7
Matīss Komļevs <matisskomlevs@gmail.com>
2022-03-05 21:22:58 +0200

    code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Container, Alert, Dropdown } from "react-bootstrap";
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import MovieList from "./components/MovieList";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
@@ -89,65 +90,67 @@ class App extends Component {
   render() {
     return (
       <div>
-        <MyNavbar showSearchResult={this.showSearchResult} />
-        <Container fluid className="px-4">
-          <div className="d-flex justify-content-between">
-            <div className="d-flex">
-              <h2 className="mb-4">TV Shows</h2>
-              <div className="ml-4 mt-1">
-                <Dropdown>
-                  <Dropdown.Toggle
-                    style={{ backgroundColor: "#221f1f" }}
-                    id="dropdownMenuButton"
-                    className="btn-secondary btn-sm dropdown-toggle rounded-0"
-                  >
-                    Genres
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu bg="dark">
-                    <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-th-large icons"></i>
-              <i className="fa fa-th icons"></i>
-            </div>
-          </div>
-          {this.state.error && (
-            <Alert variant="danger" className="text-center">
-              An error has occurred, please try again!
-            </Alert>
-          )}
-          {this.state.searchResults?.length > 0 && (
-            <MovieList
-              title="Search results"
-              movies={this.state.searchResults}
-            />
-          )}
-          {!this.state.error && !this.state.searchResults?.length > 0 && (
-            <>
-              <MovieList
-                title="Harry Potter"
+        <Router>
+          <MyNavbar showSearchResult={this.showSearchResult} />
+            <Container fluid className="px-4">
+             <div className="d-flex justify-content-between">
+               <div className="d-flex">
+                 <h2 className="mb-4">TV Shows</h2>
+                 <div className="ml-4 mt-1">
+                   <Dropdown>
+                     <Dropdown.Toggle
+                       style={{ backgroundColor: "#221f1f" }}
+                       id="dropdownMenuButton"
+                       className="btn-secondary btn-sm dropdown-toggle rounded-0"
+                     >
+                       Genres
+                     </Dropdown.Toggle>
+                     <Dropdown.Menu bg="dark">
+                       <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
+                       <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
+                       <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
+                     </Dropdown.Menu>
+                   </Dropdown>
+                 </div>
+               </div>
+               <div>
+                 <i className="fa fa-th-large icons"></i>
+                 <i className="fa fa-th icons"></i>
+               </div>
+             </div>
+             {this.state.error && (
+               <Alert variant="danger" className="text-center">
+                 An error has occurred, please try again!
+               </Alert>
+             )}
+             {this.state.searchResults?.length > 0 && (
+               <MovieList
+                 title="Search results"
+                 movies={this.state.searchResults}
+               />
+             )}
+             {!this.state.error && !this.state.searchResults?.length > 0 && (
+               <>
+                 <MovieList
+                   title="Harry Potter"
                 loading={this.state.loading}
-                movies={this.state.gallery1.slice(0, 6)}
-              />
-              <MovieList
-                title="The Avengers"
+                   movies={this.state.gallery1.slice(0, 6)}
+                 />
+                 <MovieList
+                   title="The Avengers"
                 loading={this.state.loading}
-                movies={this.state.gallery2.slice(0, 6)}
-              />
-              <MovieList
-                title="Star Wars"
+                   movies={this.state.gallery2.slice(0, 6)}
+                 />
+                 <MovieList
+                   title="Star Wars"
                 loading={this.state.loading}
-                movies={this.state.gallery3.slice(0, 6)}
-              />
-            </>
-          )}
-          <MyFooter />
-        </Container>
+                   movies={this.state.gallery3.slice(0, 6)}
+                 />
+               </>
+             )}
+            <MyFooter />
+          </Container>
+        </Router>
       </div>
     );
   }
